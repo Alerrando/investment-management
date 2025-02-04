69d47eb83090a2079ea0ff8350e642823552da71
Alerrando <alerrando2@gmail.com>
2025-02-04 00:56:08 -0300

    chang: changing style bag

--- src/app/investment-planner/AssetManagement/AssetManagement.tsx
+++ src/app/investment-planner/AssetManagement/AssetManagement.tsx
@@ -31,7 +31,7 @@ interface BagProps {
   assets: keyof AssetManagementProps;
 }
 export default function AssetManagement() {
-  const [activeTab, setActiveTab] = useState("Ações");
+  const [activeTab, setActiveTab] = useState<keyof AssetManagementProps>("Ações");
   const [searchQuery, setSearchQuery] = useState("");
   const [bag, setBag] = useState<BagProps[]>([]);
   const [quantity, setQuantity] = useState({});
@@ -129,7 +129,7 @@ export default function AssetManagement() {
         marketValue: asset.marketValue
           ? asset.marketValue
           : (asset.marketCap.replace("$", "").replace("M", "") * 1000000).toString(),
-        quantity: 0,
+        quantity: 1,
         assets: activeTab,
         quotation: asset.quotation ? parseFloat(asset.quotation) : asset.price,
       };
@@ -230,11 +230,12 @@ export default function AssetManagement() {
 
       {showBagContent && (
         <motion.div
-          className="hover:shadow-3xl fixed right-4 top-1/3 z-30 flex h-[420px] w-96 flex-col gap-2 rounded-2xl border-2 border-gray-200 bg-white p-5 shadow-2xl transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+          className="hover:shadow-3xl fixed right-4 top-1/3 z-30 flex h-[420px] w-96 flex-col gap-4 rounded-2xl border-2 border-gray-200 bg-white p-5 shadow-2xl transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, ease: ["easeIn", "easeOut"] }}
         >
+          {/* Cabeçalho */}
           <header className="flex w-full items-center justify-between">
             <h3 className="text-lg font-bold text-purple-600 dark:text-purple-400">Minha Bag</h3>
             <X
@@ -247,36 +248,31 @@ export default function AssetManagement() {
           {bag.length === 0 ? (
             <p className="text-sm text-gray-600 dark:text-gray-400">Nenhum ativo adicionado.</p>
           ) : (
-            <div className="h-full overflow-y-auto">
+            <div className="flex h-full flex-col gap-3 overflow-y-auto">
               {bag.map((asset) => (
                 <div
                   key={asset.name}
-                  className="group relative mt-6 flex items-center justify-between gap-4 rounded-xl border-2 border-gray-200 bg-white px-4 py-3 shadow-sm transition-all duration-300 hover:shadow-md dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  className="group relative flex items-center justify-between gap-3 rounded-lg border border-gray-200 bg-gray-50 p-3 transition-all duration-300 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
+                  {/* Ícone e nome do ativo */}
                   <div className="flex items-center gap-3">
-                    <button
-                      onClick={() => handleRemoveFromBag(asset.name)}
-                      className="absolute -right-0 -top-5 hidden rounded-full bg-red-600 p-1.5 text-white transition-all duration-300 hover:bg-red-700 group-hover:block"
+                    <div
+                      className={`flex h-8 w-8 items-center justify-center rounded-full ${!asset.image && "bg-purple-100 dark:bg-purple-900"}`}
                     >
-                      <Trash size={14} />
-                    </button>
-
-                    <div className="flex h-10 w-10 items-center justify-center rounded-full">
                       {asset.image ? (
                         <img src={asset.image} alt={asset.name} className="h-full w-full rounded-full object-cover" />
                       ) : asset.assets === "Fiis" ? (
-                        <BriefcaseBusiness size={28} className="text-purple-600 dark:text-purple-400" />
+                        <BriefcaseBusiness size={18} className="text-purple-600 dark:text-purple-400" />
                       ) : (
-                        <ChartCandlestick size={28} className="text-purple-600 dark:text-purple-400" />
+                        <ChartCandlestick size={18} className="text-purple-600 dark:text-purple-400" />
                       )}
                     </div>
-
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col">
                       <p className="text-sm font-semibold text-gray-700 dark:text-gray-300">{asset.name}</p>
-                      <p className="text-[10px] text-gray-600 dark:text-gray-400">
-                        M. Value:{" "}
+                      <p className="text-xs text-gray-600 dark:text-gray-400">
+                        Valor:{" "}
                         <span className="font-semibold text-gray-900 dark:text-gray-100">
-                          {parseFloat(asset.marketValue).toLocaleString("pt-BR", {
+                          {parseFloat(asset.quotation).toLocaleString("pt-BR", {
                             style: "currency",
                             currency: "BRL",
                           })}
@@ -285,15 +281,12 @@ export default function AssetManagement() {
                     </div>
                   </div>
 
-                  <div className="flex flex-col items-end gap-2">
-                    <p className="text-sm font-semibold text-gray-800 dark:text-gray-100">
-                      R$ {parseFloat(asset.quotation).toFixed(2)}
-                    </p>
-
-                    <div className="flex items-center gap-2">
+                  {/* Controle de quantidade e remoção */}
+                  <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-1">
                       <button
                         onClick={() => handleQuantityChange(asset.name, Math.max((quantity[asset.name] || 0) - 1, 0))}
-                        className="rounded-lg bg-gray-200 p-1.5 text-gray-700 transition-all duration-300 hover:bg-gray-300 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
+                        className="rounded-full bg-gray-200 p-1 text-gray-700 transition-all duration-300 hover:bg-gray-300 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
                       >
                         <MinusCircle size={12} />
                       </button>
@@ -306,19 +299,37 @@ export default function AssetManagement() {
                             handleQuantityChange(asset.name, value);
                           }
                         }}
-                        className="w-8 appearance-none rounded-lg border border-gray-300 bg-transparent py-1 text-center text-sm text-gray-700 transition-all duration-300 focus:border-purple-600 focus:ring-2 focus:ring-purple-600 dark:border-gray-700 dark:bg-gray-900 dark:text-white dark:focus:border-purple-400 dark:focus:ring-purple-400"
+                        className="w-6 appearance-none rounded-lg border border-gray-300 bg-transparent py-1 text-center text-xs text-gray-700 transition-all duration-300 focus:border-purple-600 focus:ring-2 focus:ring-purple-600 dark:border-gray-700 dark:bg-gray-900 dark:text-white dark:focus:border-purple-400 dark:focus:ring-purple-400"
                       />
-
                       <button
                         onClick={() => handleQuantityChange(asset.name, (quantity[asset.name] || 0) + 1)}
-                        className="rounded-lg bg-gray-200 p-1.5 text-gray-700 transition-all duration-300 hover:bg-gray-300 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
+                        className="rounded-full bg-gray-200 p-1 text-gray-700 transition-all duration-300 hover:bg-gray-300 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
                       >
                         <Plus size={12} />
                       </button>
                     </div>
+                    <button
+                      onClick={() => handleRemoveFromBag(asset.name)}
+                      className="rounded-full bg-red-100 p-1 text-red-600 transition-all duration-300 hover:bg-red-200 dark:bg-red-900 dark:text-red-400 dark:hover:bg-red-800"
+                    >
+                      <Trash size={12} />
+                    </button>
                   </div>
                 </div>
               ))}
+            </div>
+          )}
+
+          {bag.length > 0 && (
+            <div className="mt-4 border-t border-gray-200 pt-4 dark:border-gray-700">
+              <p className="text-sm font-semibold text-gray-700 dark:text-gray-300">
+                Total:{" "}
+                <span className="text-purple-600 dark:text-purple-400">
+                  {bag
+                    .reduce((total, asset) => total + asset.quotation * (quantity[asset.name] || 0), 0)
+                    .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                </span>
+              </p>
             </div>
           )}
         </motion.div>
