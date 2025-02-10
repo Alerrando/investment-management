1d913e8572d0e29fc238c4041312f10f8417fad3
Alerrando <alerrando2@gmail.com>
2025-02-10 12:32:50 -0300

    chang: changing src/app/stocks/page.tsx

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -14,16 +14,31 @@ export default function AllStocksPage() {
 
       <div className="h-full overflow-y-auto">
         <table className="h-full w-full table-auto border-collapse text-left text-sm text-gray-600 dark:text-gray-300">
-          <thead className="sticky top-0 z-10 bg-gray-100 dark:bg-gray-800">
+          <thead className="sticky top-0 z-10 bg-gray-800 text-white dark:bg-gray-900">
             <tr>
-              <th className="px-6 py-3">Papel</th>
-              <th className="px-6 py-3">Cotação</th>
-              <th className="px-6 py-3">P/L</th>
-              <th className="px-6 py-3">P/VP</th>
-              <th className="px-6 py-3">Dividend Yield</th>
-              <th className="px-6 py-3">Margem Líquida</th>
-              <th className="px-6 py-3">ROE</th>
-              <th className="px-6 py-3">Valor de Mercado</th>
+              <th className="px-6 py-3 font-semibold">Nome</th>
+              <th className="px-6 py-3 font-semibold">Cotação</th>
+              <th className="px-6 py-3 font-semibold">P/L</th>
+              <th className="px-6 py-3 font-semibold">P/VP</th>
+              <th className="px-6 py-3 font-semibold">PSR</th>
+              <th className="px-6 py-3 font-semibold">Dividend Yield</th>
+              <th className="px-6 py-3 font-semibold">P/Ativo</th>
+              <th className="px-6 py-3 font-semibold">P/Capital de Giro</th>
+              <th className="px-6 py-3 font-semibold">P/EBIT</th>
+              <th className="px-6 py-3 font-semibold">P/Ativo Circulante Líquido</th>
+              <th className="px-6 py-3 font-semibold">EV/EBIT</th>
+              <th className="px-6 py-3 font-semibold">EV/EBITDA</th>
+              <th className="px-6 py-3 font-semibold">Margem EBIT</th>
+              <th className="px-6 py-3 font-semibold">Margem Líquida</th>
+              <th className="px-6 py-3 font-semibold">Liquidez Corrente</th>
+              <th className="px-6 py-3 font-semibold">ROIC</th>
+              <th className="px-6 py-3 font-semibold">ROE</th>
+              <th className="px-6 py-3 font-semibold">Liquidez 2 Meses</th>
+              <th className="px-6 py-3 font-semibold">Patrimônio Líquido</th>
+              <th className="px-6 py-3 font-semibold">Dívida Líquida/Patrimônio</th>
+              <th className="px-6 py-3 font-semibold">Crescimento Receita (5 anos)</th>
+              <th className="px-6 py-3 font-semibold">Dívida Líquida/EBITDA</th>
+              <th className="px-6 py-3 font-semibold">Valor de Mercado</th>
             </tr>
           </thead>
 
@@ -36,33 +51,70 @@ export default function AllStocksPage() {
                     className="border-b border-b-[#F2F2F2] p-2 transition-colors hover:bg-[#F2F2F2] dark:border-b-[#555] dark:hover:bg-[#444444]"
                   >
                     <TableCell className="pl-4">
-                      <div className="flex h-full items-center justify-start gap-2">
-                        <ChartCandlestick className="h-8 w-8" />
-                        <div className="flex h-full flex-col justify-between py-2">
-                          <h2 className="text-[10px]">{stock.paper}</h2>
+                      <div className="flex items-center gap-2">
+                        <ChartCandlestick className="h-8 w-8 text-indigo-600" />
+                        <div className="flex flex-col py-2">
+                          <h2 className="text-sm font-medium text-gray-800 dark:text-gray-200">{stock.paper}</h2>
                         </div>
                       </div>
                     </TableCell>
 
-                    <td className="px-4 py-3">R$ {parseFloat(stock.quotation).toFixed(2)}</td>
+                    <td className="whitespace-nowrap px-6 py-4 text-right text-gray-800 dark:text-white">
+                      R$ {parseFloat(stock.quotation).toFixed(2)}
+                    </td>
 
                     <td className="px-6 py-4">{parseFloat(stock.pL).toFixed(2)}</td>
 
                     <td className="px-6 py-4">{parseFloat(stock.pVp).toFixed(2)}</td>
 
+                    <td className="px-6 py-4">{parseFloat(stock.psr).toFixed(2)}</td>
+
                     <TableCell className="pl-4">
                       <div className="flex w-fit flex-col items-end">
-                        <span>{stock.dividend}</span>
+                        <span className="font-semibold text-green-500">{stock.dividend}</span>
                       </div>
                     </TableCell>
 
+                    <td className="px-6 py-4">{parseFloat(stock.pActive).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.pWorkCapital).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.pEbit).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.pLiquidCurrentAssets).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.evEbit).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.evEbitda).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{stock.ebitMargin}</td>
+
                     <td className="px-6 py-4">{stock.liquidMargin}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.liquidCurrent).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{stock.roic}</td>
 
                     <td className="px-6 py-4">{stock.roe}</td>
 
+                    <td className="px-6 py-4">{stock.liquid2Month}</td>
+
+                    <td className="px-6 py-4">
+                      {parseFloat(stock.liquidWorth).toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.liquidDebtEquity).toFixed(2)}</td>
+
+                    <td className="px-6 py-4">{stock.revenueGrowth5Years}</td>
+
+                    <td className="px-6 py-4">{parseFloat(stock.liquityDebtEbitida || 0.0).toFixed(2)}</td>
+
                     <TableCell className="pl-4">
-                      <div className="flex w-fit flex-col items-end">
-                        <span>
+                      <div className="flex flex-col items-end">
+                        <span className="font-semibold text-green-500">
                           {parseFloat(stock.marketValue).toLocaleString("pt-BR", {
                             style: "currency",
                             currency: "BRL",
